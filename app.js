354764a98e01cb8561e058dd67299c771395c336
Johnny <jahongirhakimov677@gmail.com>
2025-04-28 18:33:20 +0500

    added slider and question & answer block

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 let que = document.querySelectorAll('.que')
 let ans = document.querySelectorAll('.answer')
+let thatIcon = document.querySelectorAll('.downAngle');
 
 for (let i = 0; i < ans.length; i++) {
     que[i].addEventListener('click', ()=>{
         ans[i].classList.toggle('active')
+        thatIcon[i].classList.toggle('active')
     })
 }
